7cfe26723c9b37ad1ae14787da16197894d8a894
FragosoNeto <112201260+FragosoNeto@users.noreply.github.com>
2022-09-06 21:16:53 -0300

    Add files via upload

---
+++ Bomdia.py
@@ -0,0 +1,3 @@
+#Crie um programa em Python que pergunte o nome do usuário e imprima um bom dia com o nome do usuário. Dica: você pode utilizar o método .format() ou uma concatenação de string, por exemplo.
+n=str(input('Informe seu nome:'))
+print(f'Bom dia,{n}')
